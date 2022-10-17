b3554df4a40395137a7d2c7ad859628b78af5832
Rizonya <zcc1911@gmail.com>
2022-10-17 12:49:47 +0300

    hover duration()()

--- src/routes/CustomButton.svelte
+++ src/routes/CustomButton.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	export let value: string = '';
 	export let Class: string =
-		'text-3xl hover:bg-amber-200 w-24 justify-center flex flex-col items-center h-24  rounded-xl cursor-pointer  bg-amber-300 ';
+		'text-3xl hover:bg-amber-200  w-24  justify-center flex flex-col items-center h-24  rounded-xl cursor-pointer  bg-amber-300 ';
 </script>
-
-<div on:click on:keydown={() => {}} class={Class + 'hover:bg-amber-200'}>
+<div on:click on:keydown={() => {}} class={Class + 'hover:bg-amber-200 duration-200'}>
 	<p class="flex flex-row">{value}</p>
 	<slot />
 </div>
